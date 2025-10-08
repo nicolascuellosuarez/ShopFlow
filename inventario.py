0b93a9662892e954361a627ce65f5c14543d6524
nicolascuellosuarez <jaimecuellosuarez@gmail.com>
2025-10-07 23:02:16 -0500

    Update inventario.py

--- inventario.py
+++ inventario.py
@@ -4,33 +4,30 @@ class Inventario:
     def __init__(self):
         self.__productos: dict[str, Producto] = {}
     
-    def agregar_producto(self, producto):
+    def agregar_producto(self, producto) -> str:
         if producto.get_id() not in self.__productos:
             self.__productos[producto.get_id()] = producto
-            return True
-        return False
+            return f"Se agregó el producto exitosamente"
+        return f"No se pudo agregar el producto"
     
-    def eliminar_producto(self, id_producto):
+    def eliminar_producto(self, id_producto) -> str:
         if id_producto in self.__productos:
             del self.__productos[id_producto]
             return True
         return False
     
-    def buscar_por_id(self, id_producto):
-        return self.__productos.get(id_producto)
+    def buscar_por_id(self, id_producto) -> dict | str:
+        if id_producto in self.__productos.items():
+            return self.__productos.get(id_producto)
+        return f"Producto no encontrado."
     
-    def buscar_por_nombre(self, nombre):
+    def buscar_por_nombre(self, nombre) -> list:
         resultados = []
         for producto in self.__productos.values():
             if nombre.lower() in producto.get_nombre().lower():
                 resultados.append(producto)
         return resultados
     
-    def actualizar_stock(self, id_producto, nueva_cantidad):
-        producto = self.buscar_por_id(id_producto)
-        if producto:
-            return producto.actualizar_stock(nueva_cantidad)
-        return False
     
     def filtrar_por_categoria(self, categoria):
         resultados = []
